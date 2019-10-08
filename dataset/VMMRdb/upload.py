07600aac0f9982752a5b003280316b1e14bbfb02
contaconta <conta1031@gmail.com>
2019-10-08 17:56:11 +0900

    added new option

--- dataset/VMMRdb/upload.py
+++ dataset/VMMRdb/upload.py
@@ -8,12 +8,22 @@ from abeja.datalake import Client as DatalakeClient
 from abeja.datasets import Client as DatasetsClient
 
 
-def build_props(image_root_dir):
-    image_list = [p.name for p in image_root_dir.iterdir()]
-    labels = [dict(label_id=i, label=name) for i, name in enumerate(image_list)]
-    label2id = {name:i for i, name in enumerate(image_list)}
+def build_props(image_root_dir, min_num_image_per_class):
+    image_list = []
+    for p in image_root_dir.iterdir():
+        num_images = len(list(p.iterdir()))
+        label = p.name
+        if min_num_image_per_class >= num_images:
+            print(f'skip label {label}, num_images: {num_images}')
+            continue
+        image_list.append(label)
+
+    labels = [dict(label_id=i, label=name)
+              for i, name in enumerate(image_list)]
+    label2id = {name: i for i, name in enumerate(image_list)}
     category = {'labels': labels, 'category_id': 0, 'name': 'VMMRdb'}
     props = {"categories": [category]}
+    print(f'num labels: {len(labels)}')
     return props, label2id
 
 
@@ -31,16 +41,17 @@ def upload_image(channel, dataset, label_id, path):
         'label_id': label_id
     }
     attributes = {'classification': [data]}
-    dataset_item = dataset.dataset_items.create(source_data=source_data, attributes=attributes)
+    dataset_item = dataset.dataset_items.create(
+        source_data=source_data, attributes=attributes)
     return dataset_item
 
 
-def upload_images(channel, dataset, image_root_dir:Path, label2id:Dict[str, int], max_workers):
-    
+def upload_images(channel, dataset, image_root_dir: Path, label2id: Dict[str, int], min_num_image_per_class, max_workers):
+
     def _upload(label, path):
         label_id = label2id[label]
         return upload_image(channel, dataset, label_id, path)
-    
+
     executer = Executer(max_workers=max_workers)
     future_pool = []
     total = len(list(image_root_dir.iterdir()))
@@ -50,31 +61,42 @@ def upload_images(channel, dataset, image_root_dir:Path, label2id:Dict[str, int]
         future_pool = []
         for image_dir in tqdm(image_root_dir.iterdir(), total=total, position=0):
             label = image_dir.name
+
             def _f(p):
                 return _upload(label, p)
             num_images = len(list(image_dir.iterdir()))
-            future_pool  += list(tqdm(executer.map(_f, image_dir.iterdir()), total=num_images, position=1))
+            if min_num_image_per_class >= num_images:
+                print(f'skip {label}, num_images: {num_images}')
+                continue
+            future_pool += list(tqdm(executer.map(_f,
+                                                  image_dir.iterdir()), total=num_images, position=1))
     return future_pool
 
 
-def main(image_root_dir, channel_id, dataset_name, max_workers):
+def main(image_root_dir, channel_id, dataset_name, max_workers, min_num_image_per_class):
     image_root_dir = Path(image_root_dir)
-    props, label2id = build_props(image_root_dir)
-    
+    props, label2id = build_props(image_root_dir, min_num_image_per_class)
+
     datakale_client = DatalakeClient()
     channel = datakale_client.get_channel(channel_id)
 
     datasets_client = DatasetsClient()
-    dataset = datasets_client.datasets.create(name=dataset_name, type='classification', props=props)
-    ret = upload_images(channel, dataset, image_root_dir, label2id, max_workers=max_workers)
+    dataset = datasets_client.datasets.create(
+        name=dataset_name, type='classification', props=props)
+    ret = upload_images(channel, dataset, image_root_dir, label2id, max_workers=max_workers,
+                        min_num_image_per_class=min_num_image_per_class)
     print('finished!')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--image_root_dir', default='VMMRdb', type=str)
-    parser.add_argument('--channel_id', help='channel_id to upload images', type=str)
-    parser.add_argument('--dataset_name', help='dataset name to be created', default='VMMRdb', type=str)
+    parser.add_argument(
+        '--channel_id', help='channel_id to upload images', type=str)
+    parser.add_argument(
+        '--dataset_name', help='dataset name to be created', default='VMMRdb', type=str)
     parser.add_argument('--max_workers', default=1, type=int)
+    parser.add_argument('--min_num_image_per_class', default=1, type=int)
     args = parser.parse_args()
-    main(args.image_root_dir, args.channel_id, args.dataset_name, args.max_workers)
-    
+    main(args.image_root_dir, args.channel_id,
+         args.dataset_name, args.max_workers, args.min_num_image_per_class)
